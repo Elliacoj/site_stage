f94e936ecf0b423965e518d842f965064fe0f64e
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-03-18 13:22:04 +0100

    Modif: modif de recherche check box de "name" à "id"

--- update.php
+++ update.php
@@ -2,8 +2,8 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/require.php";
 
 if(isset($_POST['checkId'])) {
-    $id = trim($_POST['checkId']);
-    $check = $_POST['checkValue'];
+    $id = strip_tags(trim($_POST['checkId']));
+    $check = strip_tags(trim($_POST['checkValue']));
 
     $category = new CategoryController();
     $category->updateCategory("default_visibility", $check, $id);
