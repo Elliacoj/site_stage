e9d824cd22b57d0f644ccaa891556284feb779ac
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-03-18 11:06:22 +0100

    Modif: suppression de la page display.js

--- view/elements/footer.php
+++ view/elements/footer.php
@@ -9,9 +9,6 @@
         <script src="./scripts/dark_mode.js"></script>
         <script src="./scripts/nav_mobile_version.js"></script>
 
-        <?php if(isset($_SESSION['role']) && $_SESSION['role'] !== "administrateur") {?>
-            <script src="./scripts/display.js"></script>
-        <?php } ?>
 </body>
 
 </html>
