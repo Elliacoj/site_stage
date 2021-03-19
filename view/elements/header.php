dc87e951b64f5dac2264aad3b82baf9b64c743a0
ShyroVO <Bosquet.laurie@outlook.com>
2021-03-19 15:23:47 +0100

    19/03 : icons cours option

--- view/elements/header.php
+++ view/elements/header.php
@@ -9,6 +9,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/require.php";
     <title>App</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/f8e4a7ab95.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="./elements/NG_LOGO.ico">
     <link href="./styles/commun.css" rel="stylesheet">
     <link id="pages_style" href="./styles/light.css" rel="stylesheet">
     <link href="./styles/mobile_version.css" rel="stylesheet">
